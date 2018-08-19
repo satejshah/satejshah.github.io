daaab0949df8aa42673f95b94ea64f864bea2bc7
Satej <satejshah@gmail.com>
2018-08-18 19:18:59 -0500

    35

--- temp.php
+++ temp.php
@@ -1,23 +1,18 @@
 <?php
 
-//require '/phpmailer/PHPMailerAutoload.php';
-if(isset($_POST["submit"])) {
-    echo "***** Satej Shah *****\n";
-}
-/* require_once "Mail.php";
-if(isset($_POST["submit"])) {
-$recipient = "satejshah@gmail.com"; //my email
-echo $subject = 'Email message from Point Plumbing';
-echo $name = $_POST ["name"];
-echo $email = $_POST["email"];
-echo  $message = $_POST["comments"];
-
-$mailBody="Name: $name\nEmail: $email\n\n$message"; 
-
-mail($recipient, $subject, $mailBody, "From: $name <$email>");
-
-echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
-
-}*/
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$recipient = "satejshah@gmail.com";
+$subject = $_POST['subject'];
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+ob_start();
+    echo '
+    <script type="text/javascript">
+    alert("Thank you for your message, we will get back to you as quickly as possible!");
+    window.location = "http://otcr.illinois.edu/";
+    </script>';
+    ob_end_flush();
 	
 ?>
